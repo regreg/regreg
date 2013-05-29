d8f3e32def43119cc3ed0ebe2f3c55dfc4bc05f9
Jonathan Taylor <jonathan.taylor@stanford.edu>
2013-05-29 09:10:25 -0700

    adding tests to package

--- code/regreg/setup.py
+++ code/regreg/setup.py
@@ -14,19 +14,11 @@ def cython_extension(srcfile):
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('regreg',parent_package,top_path)
-#     config.add_extension('atoms.projl1_cython',
-#                          sources = ["atoms/projl1_cython.c"],
-#                          )
-#     config.add_extension('atoms.mixed_lasso_cython',
-#                          sources = ["atoms/mixed_lasso_cython.c"],
-#                          )
-#     config.add_extension('atoms.piecewise_linear',
-#                          sources = ["atoms/piecewise_linear.c"],
-#                          )
     config.add_subpackage('atoms')
     config.add_subpackage('affine')
     config.add_subpackage('problems')
     config.add_subpackage('smooth')
+    config.add_subpackage('tests')
 
     return config
 
