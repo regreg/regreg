887dc1b703851f1b9f764390ac9217a2d1e6aeb7
Jonathan Taylor <jonathan.taylor@stanford.edu>
2015-09-16 17:38:31 -0700

    new problems test directory

---
+++ regreg/problems/tests/__init__.py
@@ -0,0 +1,6 @@
+import test_conjugate
+import test_container
+import test_nesta
+import test_nesta_path
+import test_smooth_conjugate
+import test_tfocs
--- regreg/tests/__init__.py
+++ regreg/tests/__init__.py
@@ -4,3 +4,4 @@ Tests of regreg package
 
 from regreg.affine.tests import *
 from regreg.atoms.tests import *
+from regreg.problems.tests import *
