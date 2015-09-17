983b1af987cfba14a11a78f63993346b7b74924f
Jonathan Taylor <jonathan.taylor@stanford.edu>
2015-09-16 20:24:08 -0700

    BF import

--- regreg/atoms/tests/__init__.py
+++ regreg/atoms/tests/__init__.py
@@ -1,7 +1,7 @@
-import (test_cones,
-        test_seminorms,
-        test_linear_constraints,
-        test_group_lasso,
-        test_matrix_norms,
-        test_weighted_atoms)
+from regreg.atoms.tests import (test_cones,
+                                test_seminorms,
+                                test_linear_constraints,
+                                test_group_lasso,
+                                test_matrix_norms,
+                                test_weighted_atoms)
 
