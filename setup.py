91b64a6c44554e7012b978e6163431bd0990232a
Matthew Brett <matthew.brett@gmail.com>
2016-03-23 16:47:53 -0700

    MAINT: refactor setup.py from dipy; use setuptools
    Add machinery for stamping pyx files, dependency checking.
    
    Add MANIFEST.in
    
    Unconditionally require setuptools; not requiring setuptools was getting
    tiring, and it's almost universal these days.

--- setup.py
+++ setup.py
@@ -3,66 +3,41 @@
 
 import os
 import sys
-from os.path import join as pjoin, dirname
-
-# BEFORE importing distutils, remove MANIFEST. distutils doesn't properly
-# update it when the contents of directories change.
-if os.path.exists('MANIFEST'): os.remove('MANIFEST')
+from os.path import join as pjoin, dirname, exists
 
 import numpy as np
 
-# Get version and release info, which is all stored in regreg/info.py
-ver_file = os.path.join('regreg', 'info.py')
-# Use exec for compabibility with Python 3
-exec(open(ver_file).read())
+# BEFORE importing distutils, remove MANIFEST. distutils doesn't properly
+# update it when the contents of directories change.
+if exists('MANIFEST'): os.remove('MANIFEST')
 
-# force_setuptools can be set from the setup_egg.py script
-if not 'force_setuptools' in globals():
-    # For some commands, use setuptools
-    if len(set(('develop', 'bdist_egg', 'bdist_rpm', 'bdist', 'bdist_dumb',
-                'bdist_mpkg', 'install_egg_info', 'egg_info',
-                'easy_install')).intersection(sys.argv)) > 0:
-        force_setuptools = True
-    else:
-        force_setuptools = False
+# Unconditionally require setuptools
+import setuptools
 
-if force_setuptools:
-    # Try to preempt setuptools monkeypatching of Extension handling when Pyrex
-    # is missing.  Otherwise the monkeypatched Extension will change .pyx
-    # filenames to .c filenames, and we probably don't have the .c files.
-    sys.path.insert(0, pjoin(dirname(__file__), 'fake_pyrex'))
-    import setuptools
 
-# We may just have imported setuptools, or we may have been exec'd from a
-# setuptools environment like pip
-if 'setuptools' in sys.modules:
-    extra_setuptools_args = dict(
-        tests_require=['nose'],
-        test_suite='nose.collector',
-        zip_safe=False,
-        extras_require = dict(
-            doc=['Sphinx>=1.0'],
-            test=['nose>=0.10.1']),
-        install_requires = [])
-    # I removed numpy and scipy from install requires because easy_install seems
-    # to want to fetch these if they are already installed, meaning of course
-    # that there's a long fragile and unnecessary compile before the install
-    # finishes.
-    # We need setuptools install command because we're going to override it
-    # further down.  Using distutils install command causes some confusion, due
-    # to the Pyrex / setuptools hack above (force_setuptools)
-    from setuptools.command import install
-else:
-    extra_setuptools_args = {}
-    from distutils.command import install
-
-# Import distutils _after_ potential setuptools import above, and after removing
+# Import distutils _after_ setuptools import, and after removing
 # MANIFEST
 from distutils.core import setup
 from distutils.extension import Extension
 
 from cythexts import cyproc_exts, get_pyx_sdist
-from setup_helpers import package_check
+from setup_helpers import (SetupDependency, read_vars_from)
+
+# Get version and release info, which is all stored in regreg/info.py
+info = read_vars_from(pjoin('regreg', 'info.py'))
+
+# Try to preempt setuptools monkeypatching of Extension handling when Pyrex
+# is missing.  Otherwise the monkeypatched Extension will change .pyx
+# filenames to .c filenames, and we probably don't have the .c files.
+sys.path.insert(0, pjoin(dirname(__file__), 'fake_pyrex'))
+# Set setuptools extra arguments
+extra_setuptools_args = dict(
+    tests_require=['nose'],
+    test_suite='nose.collector',
+    zip_safe=False,
+    extras_require = dict(
+        doc=['Sphinx>=1.0'],
+        test=['nose>=0.10.1']))
 
 # Define extensions
 EXTS = []
@@ -76,44 +51,45 @@ for modulename, other_sources in (
                                          "src"]))
 
 
-# Do our own build and install time dependency checking. setup.py gets called in
-# many different ways, and may be called just to collect information (egg_info).
-# We need to set up tripwires to raise errors when actually doing things, like
-# building, rather than unconditionally in the setup.py import or exec
 # Cython is a dependency for building extensions, iff we don't have stamped
 # up pyx and c files.
-extbuilder = cyproc_exts(EXTS, CYTHON_MIN_VERSION, 'pyx-stamps')
+build_ext, need_cython = cyproc_exts(EXTS,
+                                     info.CYTHON_MIN_VERSION,
+                                     'pyx-stamps')
 
-# Installer that checks for install-time dependencies
-class installer(install.install):
-    def run(self):
-        package_check('numpy', NUMPY_MIN_VERSION)
-        package_check('scipy', SCIPY_MIN_VERSION)
-        install.install.run(self)
+if need_cython:
+    SetupDependency('Cython', info.CYTHON_MIN_VERSION,
+                    req_type='setup_requires',
+                    heavy=False).check_fill(extra_setuptools_args)
+SetupDependency('numpy', info.NUMPY_MIN_VERSION,
+                req_type='setup_requires',
+                heavy=True).check_fill(extra_setuptools_args)
+SetupDependency('scipy', info.SCIPY_MIN_VERSION,
+                req_type='install_requires',
+                heavy=True).check_fill(extra_setuptools_args)
 
 
 cmdclass = dict(
-    build_ext=extbuilder,
-    install=installer,
+    build_ext=build_ext,
     sdist=get_pyx_sdist())
 
 
 def main(**extra_args):
-    setup(name=NAME,
-          maintainer=MAINTAINER,
-          maintainer_email=MAINTAINER_EMAIL,
-          description=DESCRIPTION,
-          long_description=LONG_DESCRIPTION,
-          url=URL,
-          download_url=DOWNLOAD_URL,
-          license=LICENSE,
-          classifiers=CLASSIFIERS,
-          author=AUTHOR,
-          author_email=AUTHOR_EMAIL,
-          platforms=PLATFORMS,
-          version=VERSION,
-          requires=REQUIRES,
-          provides=PROVIDES,
+    setup(name=info.NAME,
+          maintainer=info.MAINTAINER,
+          maintainer_email=info.MAINTAINER_EMAIL,
+          description=info.DESCRIPTION,
+          long_description=info.LONG_DESCRIPTION,
+          url=info.URL,
+          download_url=info.DOWNLOAD_URL,
+          license=info.LICENSE,
+          classifiers=info.CLASSIFIERS,
+          author=info.AUTHOR,
+          author_email=info.AUTHOR_EMAIL,
+          platforms=info.PLATFORMS,
+          version=info.VERSION,
+          requires=info.REQUIRES,
+          provides=info.PROVIDES,
           packages     = ['regreg',
                           'regreg.tests',
                           'regreg.affine',
