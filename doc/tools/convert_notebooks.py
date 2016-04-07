8610d249c1a3c306f66c6267eb22890889aef3dc
Matthew Brett <matthew.brett@gmail.com>
2016-04-07 02:08:17 -0700

    RF: refactor notebook conversion for directories
    nbconvert appears to be very unhappy with output file names that are not
    in the current directory - change to the output directory to build.

--- doc/tools/convert_notebooks.py
+++ doc/tools/convert_notebooks.py
@@ -1,7 +1,9 @@
 DESCRIP = 'Convert markdown notebook templates into built markdown'
 EPILOG = ''
 
-from os.path import join as pjoin, basename, splitext, getmtime, exists
+import os
+from os.path import (abspath, join as pjoin, basename, splitext, getmtime,
+                     exists)
 from glob import glob
 from subprocess import check_call
 
@@ -14,7 +16,6 @@ def get_parser():
                             formatter_class=RawDescriptionHelpFormatter)
     parser.add_argument('in_dir', type=str, help='input directory')
     parser.add_argument('nb_dir', type=str, help='notebook output directory')
-    parser.add_argument('md_dir', type=str, help='markdown output directory')
     parser.add_argument('--if-newer', action='store_true',
                         help='Only write markdown output if input is newer '
                         'than markdown output')
@@ -24,18 +25,17 @@ def get_parser():
 def main():
     parser = get_parser()
     args = parser.parse_args()
-    for in_file in glob(pjoin(args.in_dir, '*.md')):
-        if args.if_newer:
-            in_mod_time = getmtime(in_file)
+    in_files = [abspath(f) for f in glob(pjoin(args.in_dir, '*.md'))]
+    os.chdir(args.nb_dir)
+    for in_file in in_files:
         froot = splitext(basename(in_file))[0]
-        nb_file = pjoin(args.nb_dir, froot + '.ipynb')
-        md_file = pjoin(args.md_dir, froot + '.md')
+        nb_file = froot + '.ipynb'
+        md_file = froot + '.md'
         if args.if_newer and exists(md_file):
-            if getmtime(md_file) > in_mod_time:
+            if getmtime(md_file) > getmtime(in_file):
                 continue
         check_call(['notedown', in_file, '-o', nb_file])
         check_call(['jupyter', 'nbconvert', '--execute', '--inplace',
-                    '--output=' + nb_file,
                     nb_file, '--ExecutePreprocessor.timeout=-1'])
         check_call(['jupyter', 'nbconvert', '--to=markdown',
                     '--output=' + md_file, nb_file])
