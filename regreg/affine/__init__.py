eb9256874ec6d360995700e2d5ae9c9e534a3fe5
Jonathan Taylor <jonathan.taylor@stanford.edu>
2017-11-10 00:12:47 -0800

    trying to fix whitespace in doctest

--- regreg/affine/__init__.py
+++ regreg/affine/__init__.py
@@ -290,10 +290,10 @@ class selector(linear_transform):
     >>> np.dot(X, np.arange(5)) + offset
     array([ 30,  81, 132, 183, 234, 285])
 
-    >>> apply_to_first5.adjoint_map(np.arange(6)) # doctest: +NORMALIZE_WHITESPACE
-    array([275.,  290.,  305.,  320.,  335.,    0.,    0.,    0.,    0.,
-             0.,    0.,    0.,    0.,    0.,    0.,    0.,    0.,    0.,
-             0.,    0.])
+    >>> result = np.array([275.,  290.,  305.,  320.,  335.,    0.,    0.,    0.,    0.,
+                           0.,    0.,    0.,    0.,    0.,    0.,    0.,    0.,    0.,
+                           0.,    0.])
+    >>> np.testing.assert_allclose(result, apply_to_first5.adjoint_map(np.arange(6)))
 
     """
 
