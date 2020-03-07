947c679245a61cefe75ff3c6e20b2775dc1fa66c
Jonathan Taylor <jonathan.taylor@stanford.edu>
2020-03-06 23:49:45 -0800

    split block maps by row and columns

--- regreg/api.py
+++ regreg/api.py
@@ -6,33 +6,57 @@ from __future__ import print_function, division, absolute_import
 # Atom imports
 
 from .atoms import affine_atom as linear_atom
-from .atoms.seminorms import (l1norm, l2norm, supnorm, 
-                   positive_part, constrained_max,
-                   constrained_positive_part, max_positive_part)
-from .atoms.cones import (nonnegative, nonpositive,
-                   zero, zero_constraint, 
-                   l1_epigraph, l1_epigraph_polar,
-                   l2_epigraph, l2_epigraph_polar,
-                   linf_epigraph, linf_epigraph_polar,
-                   affine_cone as linear_cone)
-from .atoms.block_norms import (l1_l2, linf_l2, 
-                                l1_l1, linf_linf,
-                                block_sum, block_max)
-from .atoms.slope import (slope, slope_conjugate)
+
+from .atoms.seminorms import (l1norm, 
+                              l2norm, 
+                              supnorm, 
+                              positive_part, 
+                              constrained_max,
+                              constrained_positive_part, 
+                              max_positive_part)
+
+from .atoms.cones import (nonnegative, 
+                          nonpositive,
+                          zero, 
+                          zero_constraint, 
+                          l1_epigraph, 
+                          l1_epigraph_polar,
+                          l2_epigraph, 
+                          l2_epigraph_polar,
+                          linf_epigraph, 
+                          linf_epigraph_polar,
+                          affine_cone as linear_cone)
+
+from .atoms.block_norms import (l1_l2, 
+                                linf_l2, 
+                                l1_l1, 
+                                linf_linf,
+                                block_sum, 
+                                block_max)
+
+from .atoms.slope import (slope, 
+                          slope_conjugate)
+
 from .atoms.sparse_group_lasso import (sparse_group_lasso,
                                        sparse_group_lasso_dual)
 
-from .atoms.svd_norms import (nuclear_norm, operator_norm,
+from .atoms.svd_norms import (nuclear_norm, 
+                              operator_norm,
                               nuclear_norm_epigraph,
                               nuclear_norm_epigraph_polar,
                               operator_norm_epigraph,
                               operator_norm_epigraph_polar)
-from .atoms.linear_constraints import (projection, projection_complement)
-from .atoms.mixed_lasso import mixed_lasso, mixed_lasso_dual
+
+from .atoms.linear_constraints import (projection, 
+                                       projection_complement)
+
+from .atoms.mixed_lasso import (mixed_lasso, 
+                                mixed_lasso_dual)
 
 # group LASSO and variants
 
-from .atoms.group_lasso import (group_lasso, group_lasso_dual, 
+from .atoms.group_lasso import (group_lasso, 
+                                group_lasso_dual, 
                                 group_lasso_epigraph, 
                                 group_lasso_epigraph_polar, 
                                 group_lasso_dual_epigraph,
@@ -50,18 +74,29 @@ from .atoms.weighted_atoms import (l1norm as weighted_l1norm,
 
 # Affine imports
 
-from .affine import (identity, selector, affine_transform, normalize,
-                     linear_transform, composition as affine_composition,
-                     affine_sum, power_L, astransform)
+from .affine import (identity, 
+                     selector, 
+                     affine_transform, 
+                     normalize,
+                     linear_transform, 
+                     composition as affine_composition,
+                     affine_sum, 
+                     power_L, 
+                     astransform)
+
 from .affine.factored_matrix import (factored_matrix, compute_iterative_svd,
                                      nuclear_norm as factored_nuclear_norm,
                                      operator_norm as factored_operator_norm)
+
+from .affine.block_maps import (block_columns,
+                                block_rows)
 
 # Smooth imports
 
 from .smooth import (smooth_atom, 
                      affine_smooth, 
                      sum as smooth_sum)
+
 from .smooth.quadratic import (quadratic_loss, 
                                cholesky, 
                                signal_approximator,
@@ -72,6 +107,7 @@ from .smooth.glm import (glm,
                          poisson_loglike, 
                          gaussian_loglike, 
                          cox_loglike)
+
 from .smooth.mglm import (multinomial_baseline_loglike,
                           multinomial_loglike,
                           mglm,
@@ -79,14 +115,24 @@ from .smooth.mglm import (multinomial_baseline_loglike,
 
 # Problem imports
 
-from .problems.separable import separable, separable_problem
-from .problems.simple import simple_problem, gengrad, nesta, tfocs
+from .problems.separable import (separable, 
+                                 separable_problem)
+
+from .problems.simple import (simple_problem, 
+                              gengrad, 
+                              nesta, 
+                              tfocs)
+
 from .problems.container import container
+
 from .algorithms import FISTA
 
 from .problems.conjugate import conjugate
-from .problems.composite import (composite, nonsmooth as nonsmooth_composite,
-                                 smooth as smooth_composite, smooth_conjugate)
+
+from .problems.composite import (composite, 
+                                 nonsmooth as nonsmooth_composite,
+                                 smooth as smooth_composite, 
+                                 smooth_conjugate)
 
 from .problems.dual_problem import dual_problem
 
