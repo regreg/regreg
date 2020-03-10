bb6030cddd7d7f5a30f9f5d88777b13b2e37039f
Jonathan Taylor <jonathan.taylor@stanford.edu>
2020-03-09 21:44:48 -0700

    alternate method to run CV that allows pickling

--- regreg/paths/cross_validate.py
+++ regreg/paths/cross_validate.py
@@ -75,7 +75,7 @@ def cross_validate(path_obj,
     # Execute the jobs in parallel using joblib
     results = Parallel(n_jobs=n_jobs,
                        verbose=verbose,
-                       prefer="threading")(jobs)
+                       prefer="threads")(jobs)
 
     # Form the linear predictors and responses
 
@@ -88,5 +88,75 @@ def cross_validate(path_obj,
 
     return np.mean(scores, 0), np.std(scores, 0)
 
-    
+
+def cross_validate_alt(path_obj,
+                       fit_fold,
+                       fit_args,
+                       lagrange_seq,
+                       path_fitter=strong_rules,
+                       cv=5,
+                       group_labels=None,
+                       n_jobs=-1,
+                       score=lambda x,y: np.linalg.norm(x-y)**2 / y.shape[0],
+                       verbose=True,
+                       inner_tol=1.e-5):
+
+    '''
+    Cross validates a path fitter over a set of 
+    Lagrange parameters.
+
+    Adjusts coefficient of subsampled loss by fraction ntrain / nsample.
+
+    Parameters
+    ----------
+
+    path_obj : group_path
+    '''
+
+    # assumes that full data of path_obj.saturated_loss
+    # is the same as
+
+    # Compute paths for all folds 
+
+    response = path_obj.saturated_loss.data
+    nsample = response.shape[0]
+    have_sklearn = False
+    if have_sklearn:
+        cv = check_cv(cv)
+        folds = list(cv.split(range(nsample), range(nsample)))
+    else:
+        if type(cv) != type(3):
+            raise ValueError('without sklearn, cv should be an integer')
+        groups = np.floor(np.arange(nsample)  / nsample * cv).astype(np.int)
+        np.random.shuffle(groups)
+        folds = [(groups != g, groups == g)
+                 for g in np.unique(groups)]
+
+    # Loop over folds, computing path
+    # for each (train, test)
+
+    jobs = (delayed(fit_fold)(lagrange_seq,
+                              test,
+                              train,
+                              path_fitter,
+                              *fit_args)
+            for train, test in folds)
+
+    # Execute the jobs in parallel using joblib
+    results = Parallel(n_jobs=n_jobs,
+                       verbose=verbose,
+                       prefer="threads")(jobs)
+
+    # Form the linear predictors and responses
+
+    linpred_responses = [(path_obj.linpred(result['beta'], 
+                                           path_obj.X,
+                                           train_test[1]),
+                          response[train_test[1]])
+                         for train_test, result in zip(folds, results)]
+    scores = [[score(linpred.T[i].T, y) for i in range(linpred.shape[-1])] for (linpred, y) in linpred_responses]
+
+    return np.mean(scores, 0), np.std(scores, 0)
+
+
 
