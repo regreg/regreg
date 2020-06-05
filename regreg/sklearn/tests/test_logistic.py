bb4f1b436dbc82bf5bd34f60b110477b68ab34f0
Jonathan Taylor <jonathan.taylor@stanford.edu>
2020-06-04 23:06:49 -0700

    added probit and cloglog

--- regreg/sklearn/tests/test_logistic.py
+++ regreg/sklearn/tests/test_logistic.py
@@ -4,6 +4,8 @@ try:
     from sklearn.model_selection import GridSearchCV
     from .. import (logistic,
                     logistic_lagrange,
+                    logistic_classifier,
+                    logistic_classifier_lagrange,
                     )
     have_sklearn = True
 except ImportError:
@@ -63,6 +65,43 @@ def test_logistic():
                                    score_method='R2')
     y_both = np.array([y, np.ones_like(y), y_offset[:,1]]).T
     GridSearchCV(logistic_lasso_both, params, cv=3).fit(X, y_both)
+
+@np.testing.dec.skipif(not have_sklearn)
+@set_seed_for_test()
+def test_logistic_classifier():
+
+    n, p = 100, 20
+    X = np.random.standard_normal((n, p))
+    y = np.random.standard_normal(n)
+    y = y > 0
+
+    atom = l1norm
+    atom_args = {'shape':p, 'bound':3}
+
+    logistic_lasso = logistic_classifier(atom,
+                                         atom_args)
+
+    logistic_lasso.fit(X, y)
+    print(cross_validate(logistic_lasso, X, y, cv=10))
+
+    # grid search
+    params = {'atom_params':[{'shape':p,
+                              'bound': b} for b in [3, 4, 5]]}
+    lasso_cv = GridSearchCV(logistic_lasso, params, cv=3)
+    lasso_cv.fit(X, y)
+
+    logistic_lasso_weights = logistic_classifier(atom,
+                                                 atom_args,
+                                                 case_weights=True)
+
+    y_weights = np.array([y, np.ones_like(y)]).T
+    logistic_lasso_weights.fit(X, y_weights)
+
+    np.testing.assert_allclose(logistic_lasso_weights._coefs,
+                               logistic_lasso._coefs)
+
+    GridSearchCV(logistic_lasso_weights, params, cv=3).fit(X, y_weights)
+
 
 
 
